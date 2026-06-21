8683facbc7fc297336ae9d783f9a3c0ca88344cf
Edwige08 <edwige.saves@gmail.com>
2026-06-21 15:42:47 +0200

    update: input component with an icon to show or hide the password

--- packages/design-system/src/components/Input/Input.tsx
+++ packages/design-system/src/components/Input/Input.tsx
@@ -1,39 +1,67 @@
-import React, { forwardRef, useId } from 'react';
+import React, { forwardRef, useId, useState } from 'react';
 import styles from './Input.module.css';
+import { EyeSlash, Eye } from "@phosphor-icons/react";
 
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
-  wrapperClassName?: string;
+  required?: boolean;
+  type?: 'text' | 'password' | 'email' | 'number' | 'search' | 'tel' | 'url';
+  showPasswordToggle?: boolean;
 }
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(function Input(
   {
     label,
-    required = false,
+    required,
     id,
+    disabled = false,
     className,
-    wrapperClassName,
+    type,
+    showPasswordToggle,
     ...inputProps
   },
   ref,
 ) {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
   const generatedId = useId();
   const inputId = id ?? generatedId;
+  const showToggleIcon = type === "password" && (showPasswordToggle ?? true);
+  const togglePasswordVisibility = () => {
+    setIsPasswordVisible(!isPasswordVisible);
+  };
 
   return (
-    <div className={[styles.input, wrapperClassName].filter(Boolean).join(' ')}>
+    <div className={[styles.input].filter(Boolean).join(' ')}>
       <label htmlFor={inputId} className={styles.label}>
-        {label} {required && <span className={styles.required}>*</span>}
+        {label}&nbsp;{required && <span className={styles.required}>*</span>}
       </label>
 
-      <input
-        ref={ref}
-        id={inputId}
-        type={inputProps.type}
-        required={required}
-        className={[styles.inputField, className].filter(Boolean).join(' ')}
-        {...inputProps}
-      />
+      <div className={styles.inputWrapper}>
+        <input
+          ref={ref}
+          id={inputId}
+          type={showToggleIcon && isPasswordVisible ? "text" : type}
+          required={required}
+          disabled={disabled}
+          className={[
+            styles.inputField,
+            showToggleIcon ? styles.inputFieldWithToggle : "",
+            className,
+          ].filter(Boolean).join(" ")}
+          {...inputProps}
+        />
+        {showToggleIcon && (
+        <button
+          type="button"
+          className={styles.passwordToggleBtn}
+          onClick={togglePasswordVisibility}
+          disabled={disabled}
+          aria-label={isPasswordVisible ? "Masquer le mot de passe" : "Afficher le mot de passe"}
+        >
+          {isPasswordVisible ? <EyeSlash size={24} /> : <Eye size={24} />}
+        </button>
+        )}
+      </div>
     </div>
   );
 });
