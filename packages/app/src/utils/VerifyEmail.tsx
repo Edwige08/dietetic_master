f1d51520cbf45aed951f8a9f8b2d804293494379
Edwige08 <edwige.saves@gmail.com>
2026-07-03 16:42:42 +0200

    FE-03: add test for VerifyEmail and update function

--- packages/app/src/utils/VerifyEmail.tsx
+++ packages/app/src/utils/VerifyEmail.tsx
@@ -1,9 +1,13 @@
 export function verifyEmail(email: string) {
     const split_email = email.split("@")
-    if (split_email.length !== 2) {
-        return false
-    } 
-    if (split_email[0].length < 1 || split_email[1].length < 3 || email.includes(" ") || !split_email[1].includes(".")) {
+    const user_part = split_email[0]
+    const domain_part = split_email[1]
+
+    // regarde qu'il y a un seul "@"
+    if (split_email.length !== 2) return false
+
+    // vérifie que le user part et le domain part sont valides
+    if (user_part.length < 1 || domain_part.length < 3 || email.includes(" ") || !domain_part.includes(".") || (domain_part.includes('..')) || (domain_part.endsWith('.'))) {
         return false
     }
     return true
